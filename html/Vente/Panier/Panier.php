234dfc4e10b2cc3b2463a509738d2207473f8861
N0TS0UL <korentin.charreteur@gmail.com>
2026-05-12 16:37:12 +0200

    Ajout d'un dossier html à la racine du projet

---
+++ html/Vente/Panier/Panier.php
@@ -0,0 +1,21 @@
+<?php
+
+include './Produit/Produit.php';
+
+class Panier {
+    private $listeProduit = [];
+
+    /**
+     * Prend en parametre un produit déjà initialiser
+     * 
+     * @param Produit $produit La variable Produit qui a permit de remplir la page produit detaillée
+     */
+    public function ajouterProduit(Produit $produit) {
+        
+    }
+}
+
+
+
+
+?>
