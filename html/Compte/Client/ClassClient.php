d4128c5eb422d9c0df99ec4ddd21913e563eb51f
Titouan Guernion <titouan.guernion@gmail.com>
2026-05-13 09:42:33 +0200

    Ajout num tel + adresse

--- html/Compte/Client/ClassClient.php
+++ html/Compte/Client/ClassClient.php
@@ -4,18 +4,37 @@
     public $mail;
     public $nomUtilisateur;
     public $motDePasse;
+    public $numTelephone;
     public $type;
     public $id;
+    
+    // Informations de livraisons
+    public $adresse;
+    public $codePostal;
+    public $ville;
 
-    function __construct($mail, $nomUtilisateur, $motDePasse) {
+    function __construct($mail, $nomUtilisateur, $motDePasse, $numTelephone) {
         $this -> mail = $mail;
         $this -> nomUtilisateur = $nomUtilisateur;
         $this -> motDePasse = $motDePasse;
+        $this -> numTelephone = $numTelephone;
         $this -> type = 0;
     }
 
     function setId($id) {
         $this -> id = $id;
+    }
+
+    function setAdresse($adresse) {
+        $this -> adresse = $adresse;
+    }
+
+    function setCodePostal($codePostal) {
+        $this -> codePostal = $codePostal;
+    }
+
+    function setVille($ville) {
+        $this -> ville = $ville;
     }
 
     
