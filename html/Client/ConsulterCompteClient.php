fe6fdd066d0c0580d27f608b9b3a16f06897dd5a
etudiant <etudiant@pce-test>
2026-05-18 12:21:47 +0200

    Consulter compte clienr

--- html/Client/ConsulterCompteClient.php
+++ html/Client/ConsulterCompteClient.php
@@ -15,37 +15,37 @@
 
       <div class="divForm">
         <label for="picture">Photo de profil</label>
-        <img src="johncena.jpeg" alt="Photo de profil" class="profile-picture">
+        <img src="<?php echo('".$test."'); ?>" alt="Photo de profil" class="profile-picture">
       </div>
 
       <div class="divForm">
         <label for="username">Nom d'utilisateur</label>
-        <output type="text" name="username" required>John Cena</output>
+        <output type="text" name="username" required value="<?php echo('".$test."'); ?>">John Cena</output>
       </div>
     
       <div class="divForm">
         <label for="mail">Adresse mail</label>
-        <output type="email" name="mail" required>john.cena@example.com</output>
+        <output type="email" name="mail" required value="<?php echo('".$email_utilisateur."'); ?>">john.cena@example.com</output>
       </div>
 
       <div class="divForm">
         <label for="phone">Numéro de téléphone</label>
-        <output type="tel" name="phone" required>06 12 34 56 78</output>
+        <output type="tel" name="phone" required value="<?php echo('".$test."'); ?>">06 12 34 56 78</output>
       </div>
       
       <div class="divForm">
         <label for="address">Adresse postale</label>
-        <output type="text" name="address" required>5 Rue de la Paix</output>
+        <output type="text" name="address" required value="<?php echo('".$test."'); ?>">5 Rue de la Paix</output>
       </div>
 
       <div class="divForm">
         <label for="address">Code postal</label>
-        <output type="text" name="address" required>22300</output>
+        <output type="text" name="address" required value="<?php echo('".$test."'); ?>">22300</output>
       </div>
 
       <div class="divForm">
         <label for="address">Ville</label>
-        <output type="text" name="address" required>Lannion</output>
+        <output type="text" name="address" required value="<?php echo('".$test."'); ?>">Lannion</output>
       </div>
       <a href="ModifierCompteClient.php"><input type="button" value="Modifier"/></a>
     </form>
