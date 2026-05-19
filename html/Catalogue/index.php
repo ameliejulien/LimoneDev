e3f010b65fae9ff89b357d6fd849458c8f428431
SquidozDeRhum <henri457.aubree@gmail.com>
2026-05-19 22:53:20 +0200

    feat: buuuuuuuuuuuuuuuus

--- html/Catalogue/index.php
+++ html/Catalogue/index.php
@@ -8,20 +8,40 @@
 <html>
     <head>
         <title>Catalogue</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     </head>
     <body class="bg-[#fffdea]">
-        <div class="grid grid-cols-5 m-auto">
+        <div class="grille-produit">
             <?php 
                 foreach($images as $row) {
                     $imageData = stream_get_contents($row['photo_produit']);
                     $base64 = base64_encode($imageData);  
             ?>
-                <div class="flex flex-col justify-center">
-                    <img src=<?="data:image/jpeg;base64,$base64" ?> class="w-50 h-50">
-                    <h3><?= $row['nom_produit'] ?></h3>
-                </div>
+                <article class="carte-produit" data-id="<?= $row['produit_id'] ?>">
+                    <img src=<?="data:image/jpeg;base64,$base64" ?> class="w-50 h-50 object-contain m-auto mt-3">
+                    <div class="info-produit">
+                        <span class="producteur"><i class="fa-solid fa-location-dot"></i>BRETON</span>
+                        <h3><?= $row['nom_produit'] ?></h3>
+                        <span class="stock <?= $row['stock_produit'] < 1 ? "rupture" : "" ?>">
+                            <i class="fa-solid fa-circle"></i>
+                            <?= $row['stock_produit'] > 0 ? "En stock" : "Rupture de stock" ?>
+                        </span>
+                        <div class="pied-produit">
+                        <div class="prix-produit">
+                            <span class="montant"><?= explode(".", strval($row['prix_ht_produit']))[0] ?>,<span style="font-size:0.7em"><?= explode(".", strval($row['prix_ht_produit']))[1] ?><span class="monnaie"> €</span></span></span>
+                        </div>
+                        <button class="bouton-ajouter" aria-label="Ajouter au panier" data-add="<?= $row['produit_id'] ?>">
+                            <i class="fa-solid fa-basket-shopping"></i>
+                        </button>
+                        </div>
+                    </div>
+                </article>
             <?php } ?>
         </div>
     </body>
+    <script>
+        
+    </script>
 </html> 
--- lib/repo/ProduitRepo.php
+++ lib/repo/ProduitRepo.php
@@ -6,7 +6,11 @@ function trouverTousLesProduits(): array {
 
     $PDO = connecterBDD();
 
-    $query = "SELECT * FROM produit INNER JOIN photo_produit on id_produit = id_photo_produit;";
+    $query = "
+    SELECT * 
+    FROM produit 
+    INNER JOIN photo_produit on id_produit = id_photo_produit
+    ORDER BY id_produit;";
 
     return $PDO->query($query)->fetchAll();
 }
