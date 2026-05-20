926ccce11d27c0b7c67b313a416677218c2ee196
QuartzWasTaken <nnidwastaken@gmail.com>
2026-05-20 20:57:46 +0200

    changed fonts (make sure everything still works)

--- html/Paiement/index.php
+++ html/Paiement/index.php
@@ -1,4 +1,5 @@
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,55 +11,83 @@
     <form method="POST" action="./PaiementValide.php">
         <div class="divForm">
             <h2>Contact</h2>
-            <label for="prenom">Prénom</label>
-            <input type="text" name="prenom" required>
+            <div class="input-group prenom-input">
+                <label for="prenom">Prénom</label>
+                <input type="text" name="prenom" required>
+            </div>
 
-            <label for="nom">Nom</label>
-            <input type="text" name="nom" required>
+            <div class="input-group nom-input">
 
-            <label for="email">Adresse mail</label>
-            <input type="email" name="email" required>
+                <label for="nom">Nom</label>
+                <input type="text" name="nom" required>
+            </div>
+            <div class="input-group mail-group">
 
-            <label for="telephone">Numéro de téléphone</label>
-            <input type="tel" name="telephone" required placeholder="exemple : 0606060606">
+                <label for="email">Adresse mail</label>
+                <input type="email" name="email" required>
+            </div>
+            <div class="input-group tel-group">
+
+                <label for="telephone">Numéro de téléphone</label>
+                <input type="tel" name="telephone" required placeholder="Format : 0123456789">
+            </div>
         </div>
 
         <div class="divForm">
             <h2>Livraison</h2>
+            <div class="input-group">
+
+            </div>
             <label for="ville">Ville</label>
             <input type="text" name="ville" required>
+            <div class="input-group adresse-group">
+                <label for="adressePostal">Adresse postale</label>
+                <input type="text" name="adressePostal" required>
+            </div>
+            <div class="input-group code-postal-group">
 
-            <label for="adressePostal">Adresse postal</label>
-            <input type="text" name="adressePostal" required>
 
-            <label for="codePostal">Code postal</label>
-            <input type="text" name="codePostal" required>
+                <label for="codePostal">Code postal</label>
+                <input type="text" name="codePostal" required>
+            </div>
 
-            <label for="villeFacturation">Ville</label>
-            <input type="text" name="villeFacturation" required>
+            <div class="input-group ville-group">
 
-            <label for="adressePostalFacturation">Adresse de facturation</label>
-            <input type="text" name="adressePostalFacturation" required>
+                <label for="villeFacturation">Ville</label>
+                <input type="text" name="villeFacturation" required>
+            </div>
+            <div class="input-group adresse-fac-group">
 
-            <label for="codePostalFacturation">Code postal</label>
-            <input type="text" name="codePostalFacturation" required>
+                <label for="adressePostalFacturation">Adresse de facturation</label>
+                <input type="text" name="adressePostalFacturation" required>
+            </div>
+            <div class="input-group code-postal-fac-group">
+
+                <label for="codePostalFacturation">Code postal de facturation</label>
+                <input type="text" name="codePostalFacturation" required>
+            </div>
         </div>
 
         <div class="divForm">
             <h2>Informations de paiement</h2>
-            <label for="carteBancaire">Carte bancaire</label>
-            <input type="text" name="carteBancaire" required>
+            <div class="input-group">
 
-            <label for="titulaireCB">Nom du titulaire</label>
-            <input type="text" name="titulaireCB" required>
+                <label for="carteBancaire">Carte bancaire</label>
+                <input type="text" name="carteBancaire" required>
+                <div class="input-group">
 
-            <label for="codeSecretCB">Code secret</label>
-            <input type="text" name="codeSecretCB" required>
-        </div>
+                    <label for="titulaireCB">Nom du titulaire</label>
+                    <input type="text" name="titulaireCB" required>
+                    <div class="input-group">
 
-        <input type="submit" value="Valider" class="submit"/>
+                        <label for="codeSecretCB">Code secret</label>
+                        <input type="text" name="codeSecretCB" required>
+                    </div>
+
+                    <input type="submit" value="Valider" class="submit" />
     </form>
 </main>
 
 <script src="paiement.js"></script>
+
 </html>
