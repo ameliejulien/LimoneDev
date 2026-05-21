53a374c30849f61bc7c54a291f048351f99f503f
QuartzWasTaken <nnidwastaken@gmail.com>
2026-05-21 15:24:21 +0200

    fix: trust

--- html/API/Client.php
+++ html/API/Client.php
@@ -16,8 +16,6 @@ if ($data["typeRequete"] == "creation") {
     $retour = connexionClient($data);
     $data["reponse"] = $retour;
     ajouterClientCookie($data);
-<<<<<<< Updated upstream:html/API/Client.php
-
 } else if ($data["typeRequete"] == "deconnexion") {
     $retour = deconnecterClient();
     $data["reponse"] = $retour;
@@ -25,12 +23,10 @@ if ($data["typeRequete"] == "creation") {
 } else if ($data["typeRequete"] == "modificationMdp") {
     $retour = modificationMdpClient($data);
     $data["reponse"] = $retour;
-=======
 } else if ($data["typeRequete"] == "modification") {
     $retour=modifierClientBDD($data);
     $data["reponse"] = $retour;
     ajouterClientCookie($data);
->>>>>>> Stashed changes:html/API/CreerClient.php
 }
 
 
