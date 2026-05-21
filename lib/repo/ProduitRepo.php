bfbb9d5f2a5ff44572d69c089929a4b103a8f9b1
SquidozDeRhum <henri457.aubree@gmail.com>
2026-05-21 20:54:50 +0200

    feat: go to product page from Catalogue also add the product number of sales order

--- lib/repo/ProduitRepo.php
+++ lib/repo/ProduitRepo.php
@@ -22,7 +22,7 @@ function trouverLesProduits(): array {
     FROM produit 
     INNER JOIN categorie_produit on produit.id_produit = categorie_produit.id_produit
     INNER JOIN categorie on categorie_produit.id_categorie = categorie.id_categorie
-    LEFT JOIN photo_produit on produit.id_produit = photo_produit.id_photo_produit
+    LEFT JOIN photo_produit on produit.id_produit = photo_produit.id_produit
     INNER JOIN vendeur on produit.vendeur_produit = vendeur.id_vendeur
     ORDER BY produit.id_produit;";
 
