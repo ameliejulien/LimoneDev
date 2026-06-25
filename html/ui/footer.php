817560427d62305430a59c37444db8b4f573bc9c
Titouan Guernion <titouan.guernion@gmail.com>
2026-06-25 14:22:21 +0200

    mentions légales + lien footer

--- html/ui/footer.php
+++ html/ui/footer.php
@@ -4,15 +4,10 @@ require_once __DIR__ . '/../../lib/Constantes.php';
 ?>
 <footer class="footer">
     <div class="footer-interieur">
-        <?php if ($typeUtilisateur !== TYPE_VENDEUR): ?>
-            <a href="/Catalogue/index.php">
-                <img src="/ui/img/logo.png" alt="Logo Alizon" class="footer-logo" />
-            </a>
-        <?php else: ?>
-            <a href="/Catalogue/CatalogueVendeur.php">
-                <img src="/ui/img/logo.png" alt="Logo Alizon" class="footer-logo" />
-            </a>
-        <?php endif; ?>
+        <a href="/Catalogue/index.php">
+            <img src="/ui/img/logo.png" alt="Logo Alizon" class="footer-logo" />
+        </a>
         <span class="footer-copyright">© <?= date('Y') ?> Alizon — Tous droits réservés</span>
+        <a href="/Mentions-legales/index.php">Mentions légales</a>
     </div>
 </footer>
