4a57371a66bce24fb7f8f3f863cd95f0169633c9
N0TS0UL <korentin.charreteur@gmail.com>
2026-06-26 10:35:22 +0200

    fix : Generate uuid

--- limone.sql
+++ limone.sql
@@ -29,7 +29,7 @@ CREATE TABLE limone.Type (
 -- ===========================
 CREATE TABLE limone.Utilisateur (
     id_utilisateur serial PRIMARY KEY,
-    uuid_utilisateur uuid DEFAULT uuidv7(),
+    uuid_utilisateur uuid DEFAULT uuid_generate_v4(),
     nom_utilisateur varchar(30),
     email_utilisateur varchar(320),
     telephone_utilisateur char(10),
