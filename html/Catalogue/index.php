f1caa0158bdbca8993d6e61c82c845ce4c44994a
SquidozDeRhum <henri457.aubree@gmail.com>
2026-06-26 10:44:24 +0200

    fix: leaflet clustering

--- html/Catalogue/index.php
+++ html/Catalogue/index.php
@@ -213,6 +213,7 @@
     <?php if ($typeUtilisateur != TYPE_VENDEUR ) { ?>
         <!-- Leaflet -->
         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+        <script src="https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js"></script>
     <?php } ?>
     <script src="/Catalogue/catalogue.js"></script>
 </html>
